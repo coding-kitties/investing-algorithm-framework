7157113c8deebf9e3265e732e927282b97a0abc5
DUYN <marc.v.duyn@gmail.com>
2020-01-02 23:25:36 +0100

    Initialize remote loaders

--- bot/remote_loaders/strategy_remote_loader.py
+++ bot/remote_loaders/strategy_remote_loader.py
@@ -1,37 +1,34 @@
-import os
-from typing import Dict, Any, List
+import logging
+from pathlib import Path
 
+from bot.strategies import Strategy
 from bot import OperationalException
-from bot.strategy.strategy import Strategy
+from bot.constants import PLUGIN_STRATEGIES_DIR
+from bot.remote_loaders.remote_loader import RemoteLoader
+
+logger = logging.getLogger(__name__)
 
 
-class StrategyRemoteLoader:
+class StrategyRemoteLoader(RemoteLoader):
 
-    def __init__(self, config: Dict[str, Any]):
+    def load_strategy(self, strategy_class_name: str) -> Strategy:
 
-        self._strategies: List[Strategy] = None
+        logger.info("Loading remote strategies ...")
 
-        if not config.get('strategies'):
-            raise OperationalException(
-                "Could not resolve strategies, please provide the strategy "
-                "paths in your config file. You could also use de default strategies, that can be found in the "
-                "strategies directory. If you have difficulties creating your own strategies, please see the "
-                "documentation"
-            )
+        if not strategy_class_name:
+            raise OperationalException("Provided strategies has it search class name not set")
 
-        strategies = config.get('strategies')
+        strategies_dir = Path(PLUGIN_STRATEGIES_DIR)
+        modules = self.locate_python_modules(strategies_dir)
+        location = self.locate_class(modules, strategy_class_name)
+        generator = self.create_class_generators(location, strategy_class_name, Strategy)
+        strategy: Strategy = next(generator, None)
 
-        for strategy in strategies:
-            path = strategy.get('path')
-            self._strategies.append(self.load_strategy(path))
+        if strategy and issubclass(strategy, Strategy):
+            return strategy
 
-    @staticmethod
-    def load_strategy(path: str) -> Strategy:
-
-        if not path:
-            raise OperationalException("Provided strategies has it file path not set")
-
-        current_path = os.path.dirname(os.path.abspath(__file__))
-
-
+        raise OperationalException(
+            f"Impossible to load Strategy '{strategy_class_name}'. This strategy does not exist "
+            "or contains Python code errors."
+        )
 
