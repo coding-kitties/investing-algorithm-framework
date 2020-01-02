712843545b895758b4284f624ef84ff38f3266d4
DUYN <marc.v.duyn@gmail.com>
2020-01-02 23:32:11 +0100

    Add testing

--- tests/data_test.py
+++ tests/data_test.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pandas import DataFrame
 from pandas.io.json import json_normalize
 
-from bot.data.data_providers.data_provider import DataProvider
+from bot.data.data_provider.data_provider import DataProvider
 
 TICKER_LIST = 'https://financialmodelingprep.com/api/v3/company/stock/list'
 PROFILE_ENDPOINT = 'https://financialmodelingprep.com/api/v3/company/profile/{}'
