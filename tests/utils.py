8ef04efa887f80784288de8ac2f66f11734dde63
DUYN <marc.v.duyn@gmail.com>
2020-01-12 15:19:36 +0100

    Add random_numpy_data_frame function

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,12 @@
 import string
 import random
+from pandas import DataFrame
+import numpy as np
 
 
 def random_string(n) -> str:
     return ''.join(random.choice(string.ascii_lowercase) for _ in range(n))
+
+
+def random_numpy_data_frame() -> DataFrame:
+    return DataFrame(np.random.randint(1000, size=10000), columns=['p/e'])
