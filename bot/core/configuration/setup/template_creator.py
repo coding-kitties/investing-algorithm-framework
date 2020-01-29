10986ce3c878fc9ffe1ea2f0df44e07c11ed5281
DUYN <marc.v.duyn@gmail.com>
2020-01-29 22:47:37 +0100

    Add rewrite template suffixes

--- bot/core/configuration/setup/template_creator.py
+++ bot/core/configuration/setup/template_creator.py
@@ -1,9 +1,16 @@
+import os
+import stat
 from abc import ABC, abstractmethod
 
 from bot.core.configuration import Template
 
 
 class TemplateCreator(Template, ABC):
+
+    rewrite_template_suffixes = (
+        # Allow shipping invalid .py files without byte-compilation.
+        ('.py-template', '.py'),
+    )
 
     def __init__(self,  bot_project_directory: str, bot_name: str) -> None:
         super(TemplateCreator, self).__init__(bot_project_directory, bot_name)
@@ -19,4 +26,16 @@ class TemplateCreator(Template, ABC):
         """
 
         pass
+
+    @staticmethod
+    def make_writeable(filename):
+        """
+        Function that will make a file writeable.
+        """
+
+        if not os.access(filename, os.W_OK):
+            st = os.stat(filename)
+            new_permissions = stat.S_IMODE(st.st_mode) | stat.S_IWUSR
+            os.chmod(filename, new_permissions)
+
 
