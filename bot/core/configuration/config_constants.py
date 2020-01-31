3d3df0b4cb6b472df7e446d4fa5a8d35f7fb4df8
DUYN <marc.v.duyn@gmail.com>
2020-01-31 14:45:51 +0100

    Initial commit for config constants

---
+++ bot/core/configuration/config_constants.py
@@ -0,0 +1,7 @@
+# Naming conventions
+FRAMEWORK_NAME = 'investing bot framework'
+FRAMEWORK_CORE_MODULE_NAME = 'bot.core'
+
+# Setting related constants
+SETTINGS_MODULE_PATH_ENV_NAME = 'INVESTING_BOT_FRAMEWORK_SETTINGS_MODULE'
+
