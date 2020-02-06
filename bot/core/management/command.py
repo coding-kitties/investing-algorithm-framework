660aae8fc40a0311086286fd5adc965820ff3cb3
DUYN <marc.v.duyn@gmail.com>
2020-02-06 09:35:02 +0100

    Remove redundant parenthesis

--- bot/core/management/command.py
+++ bot/core/management/command.py
@@ -31,8 +31,8 @@ class CommandParser(ArgumentParser):
 
     def parse_args(self, args=None, namespace=None):
         # Catch missing argument for a better error message
-        if (self.missing_args_message and
-                not (args or any(not arg.startswith('-') for arg in args))):
+
+        if self.missing_args_message and not (args or any(not arg.startswith('-') for arg in args)):
             self.error(self.missing_args_message)
         return super().parse_args(args, namespace)
 
