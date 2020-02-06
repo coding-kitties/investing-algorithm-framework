d9b0107d270211db50a0879e71b3e4d366c7e19d
DUYN <marc.v.duyn@gmail.com>
2020-02-06 10:36:09 +0100

    Add validation for settings and data providers loading

--- bot/core/context/states/setup_state.py
+++ bot/core/context/states/setup_state.py
@@ -1,38 +1,44 @@
-import pkgutil
-from importlib import import_module
-
+from bot.core.exceptions import ImproperlyConfigured
 from bot.core.configuration import settings
 from bot.core.context.states import BotState
 from bot.core.resolvers import ClassCollector
 from bot.core.data_providers import DataProvider
-from bot.core.configuration.config_constants import SETTINGS_DATA_PROVIDER_REGISTERED_APPS, SETTINGS_BOT_PROJECT_NAME
+from bot.core.configuration.config_constants import SETTINGS_DATA_PROVIDER_REGISTERED_APPS
 
 
 class SetupState(BotState):
 
     def __init__(self, context):
         super(SetupState, self).__init__(context)
+        self.settings = None
 
     def run(self):
+        self.settings = settings
+
+        # Load the settings
+        if not settings.configured:
+            raise ImproperlyConfigured(
+                "Settings module is not specified, make sure you have setup a bot project and the bot is valid or that "
+                "you have specified the settings module in your manage.py file"
+            )
+
         # Initialize all data provider executors
         self._validate_data_providers()
 
-        # from bot.context.data_providing_state import DataProvidingState
-        # self.context.transition_to(DataProvidingState)
-        # self.context.run()
-
     def _validate_data_providers(self) -> None:
 
-        data_provider_apps_config = settings[SETTINGS_DATA_PROVIDER_REGISTERED_APPS]
+        data_provider_apps_config = self.settings[SETTINGS_DATA_PROVIDER_REGISTERED_APPS]
 
-        # Try to load all the specified modules
+        # Try to load all the specified data provider modules
         for data_provider_app in data_provider_apps_config:
-            # modules = [name for _, name, is_pkg in pkgutil.iter_modules([data_provider_app]) if not is_pkg and not name.startswith('_')]
-            print(data_provider_app)
-            module = import_module(data_provider_app)
-            print(dir(module))
             class_collector = ClassCollector(data_provider_app, class_type=DataProvider)
-            print(class_collector.instances)
+
+            if len(class_collector.instances) == 0:
+                raise ImproperlyConfigured(
+                    "Could not load data providers from package {}, are they implemented correctly?. Please make sure "
+                    "that you defined the right package or module. In the case of referring to your own defined data "
+                    "providers make sure that they can be imported".format(data_provider_app)
+                )
 
     def stop(self):
         # Stopping all services
