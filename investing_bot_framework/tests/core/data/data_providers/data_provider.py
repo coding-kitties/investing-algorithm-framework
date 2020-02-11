35a1843f943815b97304b85cc497d6343a61c4c7
DUYN <marc.v.duyn@gmail.com>
2020-02-11 12:04:14 +0100

    Add Tests

---
+++ investing_bot_framework/tests/core/data/data_providers/data_provider.py
@@ -0,0 +1,64 @@
+from typing import Dict, Any
+from unittest import TestCase
+
+from investing_bot_framework.core.data.data_providers import DataProvider
+from investing_bot_framework.core.events import Observer
+
+
+class TestDataProviderOne(DataProvider):
+
+    id = 'TestDataProviderOne'
+
+    def provide_data(self, **kwargs: Dict[str, Any]) -> Any:
+        return "data"
+
+
+class TestDataProviderTwo(DataProvider):
+
+    id = 'TestDataProviderTwo'
+
+    def provide_data(self, **kwargs: Dict[str, Any]) -> Any:
+        return "data"
+
+
+class TestObserver(Observer):
+
+    def __init__(self) -> None:
+        self.update_count = 0
+
+    def update(self, observable, **kwargs) -> None:
+        self.update_count += 1
+
+
+class DataProviderSetup(TestCase):
+
+    def test(self):
+        data_provider_one = TestDataProviderOne()
+
+        self.assertIsNotNone(data_provider_one.id)
+        self.assertEqual(data_provider_one.get_id(), TestDataProviderOne.id)
+
+        observer = TestObserver()
+        data_provider_one.add_observer(observer)
+
+        # Run the data provider
+        data_provider_one.start()
+
+        # Observer must have been updated
+        self.assertEqual(observer.update_count, 1)
+
+        data_provider_two = TestDataProviderTwo()
+
+        self.assertIsNotNone(data_provider_two.id)
+        self.assertEqual(data_provider_two.get_id(), TestDataProviderTwo.id)
+
+        data_provider_two.add_observer(observer)
+
+        # Run the data provider
+        data_provider_two.start()
+
+        # Observer must have been updated
+        self.assertEqual(observer.update_count, 2)
+
+        # Id´s must be different
+        self.assertNotEqual(TestDataProviderOne.id, TestDataProviderTwo.id)
