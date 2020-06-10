77f5f47e9ab300eac5d36eabe0a709d832e5207b
sadays <sarah.vetter@gmx.net>
2020-06-10 12:44:18 +0200

    Add tests data base resolver

--- investing_bot_framework/core/resolvers/database_resolver.py
+++ investing_bot_framework/core/resolvers/database_resolver.py
@@ -1,25 +1,62 @@
 import os
+from typing import Any
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Query, class_mapper, sessionmaker, scoped_session
+from sqlalchemy.orm import Query, class_mapper, sessionmaker, scoped_session, Session
 from sqlalchemy.orm.exc import UnmappedClassError
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.orm.exc import DetachedInstanceError
+from sqlalchemy.exc import DatabaseError
+
 
 from investing_bot_framework.core.configuration import settings
 from investing_bot_framework.core.configuration.config_constants import BASE_DIR, DATABASE_NAME
 from investing_bot_framework.core.exceptions import DatabaseOperationalException
 
-from typing import Any
-from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm.exc import DetachedInstanceError
-from sqlalchemy.exc import DatabaseError
+
+class _SessionProperty:
+    """
+    Wrapper for session property of a Model
+
+    To make sure that each thread gets an scoped session, a new scoped session is created if a new thread
+    accesses the session property of a Model.
+    """
+    def __init__(self, db):
+        self.db = db
+
+    def __get__(self, instance, owner):
+        return self.db.session
+
+
+class _QueryProperty:
+    """
+    Wrapper for query property of a Model
+
+    This wrapper makes sure that each model gets a Query object with a correct session corresponding to its thread.
+    """
+    def __init__(self, db):
+        self.db = db
+
+    def __get__(self, instance, owner):
+
+        try:
+            mapper = class_mapper(owner)
+            if mapper:
+                return owner.query_class(mapper, session=self.db.session)
+
+        except UnmappedClassError:
+            return None
 
 
 class Model(object):
     table_name = None
     session = None
     query_class = None
-    query = None
+    _query = None
+
+    @property
+    def query(self) -> Query:
+        return self._query
 
     @declared_attr
     def __tablename__(cls):
@@ -72,71 +109,12 @@ class Model(object):
         return f"<{self.__class__.__name__} {id(self)}>"
 
 
-class BaseQuery(Query):
-    """
-    SQLAlchemy :class:`~sqlalchemy.orm.query.Query` subclass with convenience methods for querying in a web
-    application. This is the default :attr:`~Model.query` object used for models, and
-    exposed as :attr:`~SQLAlchemy.Query`. Override the query class for an individual model by subclassing
-    this and setting :attr:`~Model.query_class`.
-    """
-
-    def get_or_404(self, ident, description=None):
-        """Like :meth:`get` but aborts with 404 if not found instead of returning ``None``."""
-
-        rv = self.get(ident)
-        if rv is None:
-            raise Exception()
-        return rv
-
-    def first_or_404(self, description=None):
-        """Like :meth:`first` but aborts with 404 if not found instead of returning ``None``."""
-
-        rv = self.first()
-        if rv is None:
-            raise Exception()
-        return rv
-
-
-class _SessionProperty:
-    """
-    Wrapper for session property of a Model
-
-    To make sure that each thread gets an scoped session, a new scoped session is created if a new thread
-    accesses the session property of a Model.
-    """
-    def __init__(self, db):
-        self.db = db
-
-    def __get__(self, instance, owner):
-        return self.db.session
-
-
-class _QueryProperty:
-    """
-    Wrapper for query property of a Model
-
-    This wrapper makes sure that each model gets a Query object with a correct session corresponding to its thread.
-    """
-    def __init__(self, db):
-        self.db = db
-
-    def __get__(self, instance, owner):
-
-        try:
-            mapper = class_mapper(owner)
-            if mapper:
-                return owner.query_class(mapper, session=self.db.session)
-
-        except UnmappedClassError:
-            return None
-
-
 class DatabaseResolver:
 
-    def __init__(self, query_class=BaseQuery, model_class=Model):
+    def __init__(self, query_class=Query, model_class=Model):
         self._configured = False
         self.Query = query_class
-        self.Model = self.make_declarative_base(model_class)
+        self._model = self.make_declarative_base(model_class)
         self.engine = None
         self.session_factory = None
         self.Session = None
@@ -147,15 +125,15 @@ class DatabaseResolver:
         self.session_factory = sessionmaker(bind=self.engine)
         self.Session = scoped_session(self.session_factory)
 
-        if self.Model is None:
+        if self._model is None:
             raise DatabaseOperationalException("Model is not defined")
 
-        self.Model.session = _SessionProperty(self)
+        self._model.session = _SessionProperty(self)
 
-        if not getattr(self.Model, 'query_class', None):
-            self.Model.query_class = self.Query
+        if not getattr(self._model, 'query_class', None):
+            self._model.query_class = self.Query
 
-        self.Model.query = _QueryProperty(self)
+        self._model.query = _QueryProperty(self)
 
     def initialize(self):
         base_dir = settings.get(BASE_DIR)
@@ -182,3 +160,21 @@ class DatabaseResolver:
 
         model = declarative_base(cls=model_class)
         return model
+
+    @property
+    def session(self) -> Session:
+        """
+        Returns scoped session of an Session object
+        """
+        return self.Session()
+
+    @property
+    def model(self) -> Model:
+        return self._model
+
+    def initialize_tables(self):
+        self._model.metadata.create_all(self.engine)
+
+
+
+
--- investing_bot_framework/tests/core/resolvers/test_database_resolver.py
+++ investing_bot_framework/tests/core/resolvers/test_database_resolver.py
@@ -1,17 +1,21 @@
 import os
 from unittest import TestCase
+from sqlalchemy import Column, String, Integer
 
-from investing_bot_framework.core.resolvers import DatabaseResolver
-from investing_bot_framework.tests.resources import BaseTestMixin
+from investing_bot_framework.tests.resources import BaseTestMixin, utils
 from investing_bot_framework.core.configuration import settings
 from investing_bot_framework.core.extensions import db
+
+
+class TestModel(db.model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
 
 
 class TestDatabaseResolverConfiguration(TestCase, BaseTestMixin):
 
     def setUp(self) -> None:
         self.initialize_environment()
-        self.db_resolver = DatabaseResolver()
 
     def test_configuration(self):
         settings.configure()
@@ -28,5 +32,33 @@ class TestDatabaseResolverConfiguration(TestCase, BaseTestMixin):
 
         if os.path.isfile(db.database_path):
             os.remove(db.database_path)
+
+
+class TestDatabaseResolverModel(TestCase, BaseTestMixin):
+
+    def setUp(self) -> None:
+        self.initialize_environment()
+        settings.configure()
+        db.configure()
+        db.initialize_tables()
+
+    def test(self) -> None:
+
+        model = TestModel(name=utils.random_string(10))
+        model.save()
+        db.session.commit()
+        self.assertEqual(1, len(TestModel.query.all()))
+
+        model = TestModel(name=utils.random_string(10))
+        model.save()
+        db.session.commit()
+        self.assertEqual(2, len(TestModel.query.all()))
+
+    def tearDown(self) -> None:
+
+        if os.path.isfile(db.database_path):
+            os.remove(db.database_path)
+
+
 
 
