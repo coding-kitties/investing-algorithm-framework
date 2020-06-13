0bc6d4efc88d7fa51f840d6b577e1150eb58818e
sadays <sarah.vetter@gmx.net>
2020-06-14 00:08:05 +0200

    Add Strategy template

---
+++ investing_bot_framework/templates/bot_project_directory/bot_project_template/strategies/strategies.py
@@ -0,0 +1,15 @@
+from investing_bot_framework.core.strategies import Strategy
+from investing_bot_framework.core.utils import TimeUnit
+
+"""
+Define here all your strategies
+"""
+
+
+class MyStrategy(Strategy):
+    time_unit = TimeUnit.SECOND
+    time_interval = 10
+    id = 'my_strategy'
+
+    def apply_strategy(self) -> None:
+        pass
