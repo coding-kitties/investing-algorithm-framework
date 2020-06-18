6cefc21438c437be6e39949ed4e5b4c997fb3584
DUYN <marcvanduyn1@gmail.com>
2020-06-18 19:34:45 +0200

    Add version tests

--- investing_bot_framework/tests/resources/standard_settings.py
+++ investing_bot_framework/tests/resources/standard_settings.py
@@ -1,10 +1,49 @@
+import os
 from pathlib import Path
 
-BOT_PROJECT_NAME = 'TEST_ALGORITHM'
+BOT_PROJECT_NAME = 'bot'
 
-BASE_DIR = str(Path(__file__).parent)
+BOT_CONTEXT_CONFIGURATION = 'bot.configuration.context'
 
-INSTALLED_DATA_PROVIDER_APPS = []
+# Change this when not in development, feature or hot-fix branch
+DEBUG = int(os.environ.get('DEBUG', True))
 
-INSTALLED_STRATEGY_APPS = []
+BASE_DIR = str(Path(__file__).parent.parent)
 
+LOG_FILE_NAME = 'log'
+
+LOG_DIR = '{}/logs'.format(BASE_DIR)
+
+LOG_PATH = "{}/{}.log".format(LOG_DIR, LOG_FILE_NAME)
+
+# if not os.path.isdir(LOG_DIR):
+#     os.mkdir(LOG_DIR)
+
+if DEBUG:
+    logging_level = "DEBUG"
+else:
+    logging_level = "INFO"
+
+# Logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(levelname)s %(asctime)s - [thread: %(threadName)-4s %(name)s] %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'level': logging_level,
+            'handlers': ['console'],
+        },
+    },
+}
