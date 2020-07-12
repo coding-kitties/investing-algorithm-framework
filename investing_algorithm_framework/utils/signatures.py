09ac4e2b9d072a6b8fdda8da4e810e2add6c7d45
DUYN <marcvanduyn1@gmail.com>
2020-07-12 20:52:14 +0200

    Add sha256 signature

---
+++ investing_algorithm_framework/utils/signatures.py
@@ -0,0 +1,17 @@
+import hmac
+import hashlib
+
+
+# Function to create a sha256 signature
+def create_sha256_signature(secret_key: str, params: str):
+
+    # Encode the secret key
+    byte_secret_key = bytearray()
+    byte_secret_key.extend(map(ord, secret_key))
+
+    # Encode the params
+    encoded_params = params.encode()
+
+    # Create signature
+    return hmac.new(byte_secret_key, encoded_params, hashlib.sha256)\
+        .hexdigest()
