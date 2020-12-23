c9df926dab8aefbd2dbc2d541dafb72e506a2dfe
mduyn <marc.vanduyn@serviceheroes.com>
2020-12-23 23:52:30 +0100

    feat(TRAVIS_CI): version

---
+++ ci/upload_package.py
@@ -0,0 +1,22 @@
+import os
+from yolk.pypi import CheeseShop
+from investing_algorithm_framework import get_version
+
+PACKAGE_NAME = 'investing_algorithm_framework'
+
+
+def get_latest_version_number(package_name):
+    pkg, all_versions = CheeseShop().query_versions_pypi(package_name)
+    if len(all_versions):
+        return all_versions[0]
+    return None
+
+
+if __name__ == "__main__":
+    released_version = get_latest_version_number(PACKAGE_NAME)
+
+    if released_version != get_version():
+        os.chdir("../")
+        os.system("python setup.py sdist bdist_wheel")
+        os.system("twine upload -r investing-algorithm-framework -p pipy_password -u pipy_username dist/*")
+        os.system("twine upload dist/*")
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="investing_algorithm_framework",
-    version="0.1.2",
+    version=get_version(),
     license="BSL-1.1",
     author="coding kitties",
     description="A framework for creating an investment algorithm",
