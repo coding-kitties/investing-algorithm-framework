f52c29a6827ef8f9fde5c6fec27cba734cb6d655
Marc van Duyn <marcvanduyn1@gmail.com>
2022-01-20 15:02:17 +0100

    Develop (#149)
    * feat(RELEASE): set version to v0.12.0
    
    * feat(PERFORMANCE): add default portfolio specifier (#132)
    
    * feat(RELEASE): release v0.13.0
    
    * feat(README): update readme
    
    * feat(AUTHORS): add authors
    
    * ref(DEBUG): remove debug
    
    * Feature/#135 (#136)
    
    * ref(DEBUG): remove debug
    
    * feat(EXPORTS): add exception exports
    
    Co-authored-by: marcvanduyn <marc.vanduyn@serviceheroes.com>
    
    * feat(TESTS): add setter check
    
    * ref(PORTFOLIO_MANAGER): make base portfolio manager abstract
    
    * ref(IMPORTS): fix imports
    
    * feat(RELEASE): make fix release
    
    * fix(BINANCE_MARKET_SERVICE): fix initialization order
    
    * feat(COMPONENTS): implement component wrapper
    
    * feat(VERSION): bump version to 0.14.0
    
    * feat(MARKET_SERVICE): delay initialization
    
    * feat(VERSION): bump version
    
    * fix(INITIALIZATION): fix initialization order
    
    * feat(VERSION): bump version
    
    * feat(VERSION): bump version
    
    * fix(VALIDATION): remove pair validation
    
    * fix(IDENTIFIER): set default value for identifier
    
    * feat(VERSION): bump version
    
    * feat(VERSION): bump version
    
    * fix(VALIDATION): add pair validation
    
    * fix(STATUS): fix status updating
    
    * feat(ORDERS): support orders retrieving
    
    * feat(VERSION): bump version
    
    * fix(POSITIONS): fix retrieve positions
    
    * feat(VERSION): bump version
    
    * fix(TESTS): fix teardown
    
    * fix(TESTS): fix teardown
    
    * Feature/models refactoring (#147)
    
    * feat(MODELS): update models
    
    * feat(MODELS): separate sql models
    
    Now models are split-up into base models and sqllite instance models.
    
    This will help us in the future with custom model implementations
    
    Co-authored-by: marcvanduyn <marc.vanduyn@serviceheroes.com>
    
    * feat(VERSION): bump version
    
    * fix(PORTFOLIO_ID): update portfolio id
    
    * feat(VERSION): bump version
    
    Co-authored-by: marcvanduyn <marc.vanduyn@serviceheroes.com>

--- tests/core/context/test_create_limit_buy_order.py
+++ tests/core/context/test_create_limit_buy_order.py
@@ -39,12 +39,7 @@ class Test(TestBase, TestOrderAndPositionsObjectsMixin):
         self.algo_app.add_order_executor(OrderExecutorTestTwo)
         self.algo_app.add_portfolio_manager(PortfolioManagerTestTwo)
         self.start_algorithm()
-
-    def tearDown(self) -> None:
-        db.session.query(SQLLitePortfolio).delete()
-        db.session.commit()
-        super(Test, self).tearDown()
-
+        
     def test(self) -> None:
         order = self.algo_app.algorithm\
             .create_limit_buy_order(
