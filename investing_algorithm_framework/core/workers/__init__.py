639730303eb112407bd8a2fb64e7766cb3e3fe25
marc <marcvanduyn1@gmail.com>
2022-02-05 14:22:29 +0100

    feat(STRATEGY): support multiple arguments

--- investing_algorithm_framework/core/workers/__init__.py
+++ investing_algorithm_framework/core/workers/__init__.py
@@ -1,5 +1,5 @@
 from investing_algorithm_framework.core.workers.worker import Worker
 from investing_algorithm_framework.core.workers.strategy_worker import \
-    StrategyWorker
+    Strategy
 
-__all__ = ['Worker', 'StrategyWorker']
+__all__ = ['Worker', 'Strategy']
