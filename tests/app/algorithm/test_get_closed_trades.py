dd5d6b5684f9aefed68661e5a570c0553fdbd31c
marcvanduyn <marcvanduyn1@gmail.com>
2023-12-04 01:10:41 +0100

    Add trade tests

---
+++ tests/app/algorithm/test_get_closed_trades.py
@@ -0,0 +1,65 @@
+import os
+
+from investing_algorithm_framework import create_app, RESOURCE_DIRECTORY, \
+    PortfolioConfiguration
+from tests.resources import TestBase, MarketServiceStub
+
+
+class Test(TestBase):
+
+    def setUp(self) -> None:
+        self.resource_dir = os.path.abspath(
+            os.path.join(
+                os.path.join(
+                    os.path.join(
+                        os.path.join(
+                            os.path.realpath(__file__),
+                            os.pardir
+                        ),
+                        os.pardir
+                    ),
+                    os.pardir
+                ),
+                "resources"
+            )
+        )
+        self.app = create_app(config={RESOURCE_DIRECTORY: self.resource_dir})
+        self.app.add_portfolio_configuration(
+            PortfolioConfiguration(
+                market="binance",
+                api_key="test",
+                secret_key="test",
+                trading_symbol="USDT"
+            )
+        )
+        self.app.container.market_service.override(MarketServiceStub())
+        self.app.initialize()
+
+    def test_get_open_trades(self):
+        self.app.algorithm.create_limit_order(
+            target_symbol="BTC",
+            price=10,
+            order_side="BUY",
+            amount=20
+        )
+        order = self.app.algorithm.get_order()
+        self.assertIsNotNone(order)
+        self.assertEqual(0, len(self.app.algorithm.get_closed_trades()))
+        order_service = self.app.container.order_service()
+        order_service.check_pending_orders()
+        self.assertEqual(0, len(self.app.algorithm.get_closed_trades()))
+        trade = self.app.algorithm.get_trades()[0]
+        self.assertEqual(10, trade.open_price)
+        self.assertEqual(20, trade.amount)
+        self.assertEqual("BTC", trade.target_symbol)
+        self.assertEqual("USDT", trade.trading_symbol)
+        self.assertIsNone(trade.closed_price)
+        self.assertIsNone(trade.closed_at)
+        self.app.algorithm.create_limit_order(
+            target_symbol="BTC",
+            price=10,
+            order_side="SELL",
+            amount=20
+        )
+        order_service.check_pending_orders()
+        self.assertEqual(1, len(self.app.algorithm.get_closed_trades()))
