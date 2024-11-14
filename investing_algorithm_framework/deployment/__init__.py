5c670244a38bbc56ce00f308eee1bcfba733fd02
marcvanduyn <marcvanduyn1@gmail.com>
2024-11-14 22:59:11 +0100

    Remove unused imports

--- investing_algorithm_framework/deployment/__init__.py
+++ investing_algorithm_framework/deployment/__init__.py
@@ -1,3 +1,3 @@
-from .azure import deploy_to_azure_functions
+# from .azure import deploy_to_azure_functions
 
-__all__ = ['deploy_to_azure_functions']
+# __all__ = ['deploy_to_azure_functions']
--- investing_algorithm_framework/deployment/azure/__init__.py
+++ investing_algorithm_framework/deployment/azure/__init__.py
@@ -1,3 +1,3 @@
-from .azure_functions import deploy_to_azure_functions
+# from .azure_functions import deploy_to_azure_functions
 
-__all__ = ['deploy_to_azure_functions']
+# __all__ = ['deploy_to_azure_functions']
