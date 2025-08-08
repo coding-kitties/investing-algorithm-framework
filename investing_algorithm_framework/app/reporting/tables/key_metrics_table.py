7469f7f264edeca0d48935b739984558a0f3e2c4
marcvanduyn <marcvanduyn1@gmail.com>
2025-08-08 21:31:23 +0300

    Fix quotes

--- investing_algorithm_framework/app/reporting/tables/key_metrics_table.py
+++ investing_algorithm_framework/app/reporting/tables/key_metrics_table.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from .utils import safe_format, safe_format_date, safe_format_percentage
+from .utils import safe_format, safe_format_percentage
 
 
 def highlight_sharpe_and_sortino(row):
@@ -153,7 +153,10 @@ def create_html_key_metrics_table(results, report):
     format_str = "{:.2f}"
 
     # Format some values to percentages and floats
-    copy_results['Total Return'] = f"{safe_format(copy_results["total_net_gain"], format_str)} ({safe_format_percentage(copy_results['total_net_gain_percentage'], format_str)}%)"
+    copy_results['Total Return'] = (
+        f"{safe_format(copy_results['total_net_gain'], format_str)} "
+        f"({safe_format_percentage(copy_results['total_net_gain_percentage'], format_str)}%)"
+    )
     copy_results['CAGR'] = f"{safe_format_percentage(copy_results['cagr'],format_str)}%"
     copy_results['Sharpe Ratio'] = safe_format(copy_results['sharpe_ratio'], format_str)
     copy_results['Sortino Ratio'] = safe_format(copy_results['sortino_ratio'], format_str)
